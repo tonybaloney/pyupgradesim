df0a2b592a3c5f2f3b7602d1c0460d5b591af426
Anthony Shaw <anthony.p.shaw@gmail.com>
2023-03-14 15:22:25 +1100

    Don't use network host mode because it's not supported on macOS

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ COPY benchsite /app
 WORKDIR /app
 RUN python -m pip install -r requirements.txt
 
-CMD python manage.py migrate && python manage.py loaddata seed.json && python -m gunicorn benchsite.wsgi:application --bind 0.0.0.0:8900 -w 4
+CMD python manage.py migrate && python manage.py loaddata seed.json && python -m gunicorn benchsite.wsgi:application --bind 0.0.0.0:80 -w 4
--- profilersite/profiler/tasks.py
+++ profilersite/profiler/tasks.py
@@ -4,7 +4,7 @@ import pathlib
 def test_version(version, time=60, users=4):
     root_path = pathlib.Path.cwd().parent
     # start the server
-    subprocess.run(['docker', 'run', '--rm', '-d', '--network', 'host', "--name", f"benchmark-server-{version}", f"benchapp-{version}"], check=True)
+    subprocess.run(['docker', 'run', '--rm', '-d', '-p', "127.0.0.1:8900:80/tcp", "--name", f"benchmark-server-{version}", f"benchapp-{version}"], check=True)
     # run the tests
     subprocess.run(['python', '-m', 'profiler', f'results_{version}', str(time), str(users)], cwd=root_path, check=True)
     # stop the server
